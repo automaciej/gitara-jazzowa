a439ff16ec1cbe2673c808da2ab04f983292c19d
Maciej Bliziński <maciej.blizinski@gmail.com>
2021-08-21 20:35:58 +0100

    A simple script to deploy the site.

---
+++ utils/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -u
+
+DEST=atemoia:www/blizinski.pl
+
+hugo=hugo
+
+helpmsg() {
+  echo "$0 [ deploy | test ]"
+}
+
+case "${1:-}" in
+  deploy)
+    umask 022
+    find static -type f -exec chmod 644 {} \;
+    find static -type d -exec chmod 755 {} \;
+    echo "Cleaning the destination directory…"
+    ${hugo} --cleanDestinationDir
+    find public -type f -exec chmod 644 {} \;
+    find public -type d -exec chmod 755 {} \;
+    echo "Running rsync…"
+    rsync -a --delete public/ "${DEST}"
+    echo "Done."
+    ;;
+  test)
+    ${hugo} -w -D -F server
+    ;;
+  *)
+    helpmsg >&2
+    exit 1
+    ;;
+esac
